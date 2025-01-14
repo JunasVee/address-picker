ca24e58ab7389f602652582944ee7e0b1ba08f81
JunasVee <arjunagamers777@gmail.com>
2025-01-14 17:37:11 +0700

    updated ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Map from "@/components/Map";  // Import Map.tsx
 
 export default function HomePage() {
   const [location, setLocation] = useState<{ lat: number; lng: number; address: string } | null>(null);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Set isClient to true after the component mounts on the client-side
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    // Return a loading state or a placeholder while the component is being rendered on the server
+    return (
+      <div className="h-screen w-full flex flex-col items-center justify-center">
+        <h1 className="text-lg">Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
